a338456b925881655af0ec507dc3b62dccdf540d
asmgoni <asm.goni21@my.stjohns.edu>
2023-07-30 04:25:08 -0400

    updated links

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var element = document.getElementById("intro");
+element.parentNode.removeChild(element);
+
 setTimeout(setInterval(function () {
     var timeNow = new Date().toLocaleString();
     var timestamp = document.getElementById("timestamp");
